ac192e3e8615fed8163e31a90fd26e5e452d5a70
rafnas <rafnasrooni@gmail.com>
2024-08-23 15:07:28 +0530

    creat todo-list

---
+++ src/Home.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Creat from './Creat'
+
+function Home() {
+    const [todos,setTodo]=useState([])
+  return (
+    <div>
+
+       <h2> Todo List</h2>
+       <Creat/>
+       {
+        todos.length===0?
+        <div> <h2>No REcord</h2></div>
+        todos.map(todo=>(
+         <div>
+          {todo}
+          </div>
+        )
+
+        )
+       }
+       </div>
+  )
+}
+
+export default Home
