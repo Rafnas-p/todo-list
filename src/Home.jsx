37a27f1acd871602f17d77217e21207dced31856
rafnas-p <rafnasrooni@gmail.com>
2024-08-23 15:26:44 +0530

    todos-fetch

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,22 @@
-import React, { useState } from 'react'
-import Creat from './Creat'
+import React, { useState } from "react";
+import Creat from "./Creat";
 
 function Home() {
-    const [todos,setTodo]=useState([])
+  const [todos, setTodo] = useState([]);
   return (
     <div>
-
-       <h2> Todo List</h2>
-       <Creat/>
-       {
-        todos.length===0?
-        <div> <h2>No REcord</h2></div>
-        todos.map(todo=>(
-         <div>
-          {todo}
-          </div>
-        )
-
-        )
-       }
-       </div>
-  )
+      <h2> Todo List</h2>
+      <Creat />
+      {todos.length === 0 ? (
+        <div>
+          {" "}
+          <h2>No REcord</h2>
+        </div>
+      ) : (
+        todos.map((todo) => <div>{todo}</div>)
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
